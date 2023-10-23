84dcde64538177a60d7a5c3e29d7d7815cdef349
Elvina <99557373+Ar444ri@users.noreply.github.com>
2023-10-24 00:16:40 +0300

    Update bar.cpp
    удалила ненужное

--- SignalsAndSlots/bar.cpp
+++ SignalsAndSlots/bar.cpp
@@ -6,17 +6,6 @@ Bar::Bar( QString danStyle, QString norStyle,QProgressBar *parent): QProgressBar
 }
 
 void Bar::SetColor(int health, int percent,const QString danStyle,const QString norStyle) {
-  QString kDangerStyle =
-          "QProgressBar::chunk {background: #E6E6FA; Width: 20px; margin: 0.5px;"
-          "border: 1px solid black; border-radius:8px; Border-Radius: 4px;} "
-          "QProgressBar { text-align: center; font-size:14px; border-radius:8px; "
-          "color:black;}";
-     QString kNormalStyle =
-        "QProgressBar::chunk {background: #FFC0CB; Width: 20px; margin: 0.5px; "
-        "border: 1px solid black; border-radius:8px; Border-Radius: 4px;} "
-        "QProgressBar { text-align: center; font-size:14px; border-radius:8px; "
-        "color:black;}";
-
     if (health < percent) {
          this->setStyleSheet(danStyle);
      } else {
